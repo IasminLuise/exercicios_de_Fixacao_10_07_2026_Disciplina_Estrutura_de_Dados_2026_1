4496702952620825746d1201659b2a8b99095477
Iasmin Silva <iasminluise14@gmail.com>
2026-05-21 17:35:51 -0300

    Add main method to read and print 5 numbers

---
+++ questao14.c
@@ -0,0 +1,20 @@
+/*------------------MÉTODO PRINCIPAL---------------------*/
+/*--------------------ATIVIDADE 14-----------------------*/
+#include <stdio.h>
+
+int main (){
+    
+    int num;
+    
+    printf("Digite 5 números:\n");
+    
+    for(int i = 1; i <= 5; i++){
+        
+        scanf("%d", &num);
+        
+        printf("Número digitado: %d\n", num);
+        
+    }
+    
+    return 0;
+}

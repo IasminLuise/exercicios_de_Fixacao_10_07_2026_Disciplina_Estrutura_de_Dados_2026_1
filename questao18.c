d01a0633588cdf25a9af389c6d4919668a96e79f
Iasmin Silva <iasminluise14@gmail.com>
2026-05-23 15:42:00 -0300

    Create questao18.c

---
+++ questao18.c
@@ -0,0 +1,17 @@
+
+/*------------------MÉTODO PRINCIPAL---------------------*/
+/*--------------------ATIVIDADE 18-----------------------*/
+#include <stdio.h>
+
+int main (){
+    
+    printf("Tabuada do número 4:\n");
+    
+    for(int i = 1; i <= 10; i++){
+        
+        printf("4 x %d = %d\n", i, (4 * i));
+        
+    }
+    
+    return 0;
+}
